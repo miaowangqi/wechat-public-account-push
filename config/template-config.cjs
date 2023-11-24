ef207b7687140c707a685b1a719be8cd850062bc
miaowangqi <150899610+miaowangqi@users.noreply.github.com>
2023-11-24 16:29:52 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -2,37 +2,26 @@
 const TEMPLATE_CONFIG = [
   {
     id: '0001',
-    title: '亲爱的, 早上好',
+    title: '秀慧宝宝, 早上好',
     desc: `
-      **{{date.DATA}}**
+      🗓️{{date.DATA}}
       
       下个休息日：{{holidaytts.DATA}}
-      
       ---
-      
       城市：{{city.DATA}}
-      
-      天气：{{weather.DATA}}
-      
-      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
-      
+      天气☁️：{{weather.DATA}}
+      气温🌡️: {{min_temperature.DATA}}--{{max_temperature.DATA}}
       风向: {{wind_direction.DATA}}
-      
       风级: {{wind_scale.DATA}}
-      
-      {{comprehensive_horoscope.DATA}}
-      
       ---
-      
-      今天是我们相识的第{{love_day.DATA}}天
-      
+      今天是我们相爱的第{{love_day.DATA}}天
       {{birthday_message.DATA}}
-      
       ---
+      💗：{{earthy_love_words.DATA}}
+      每日一句😋
+      中文：{{note_ch.DATA}}
+      English：{{note_en.DATA}}
       
-      {{moment_copyrighting.DATA}}
-      
-      {{poetry_title.DATA}} {{poetry_content.DATA}}
     `
   },
   {
